2c8c32fcd22fb9e329e368f73a43b36d0999fde4
JC <ezjcd101@gmail.com>
2022-04-28 18:37:44 +0800

    final commit

--- FoodWarehouseAPI/views.py
+++ FoodWarehouseAPI/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import viewsets
+from . serializers import fstockSerializer
+from . models import fstock
 
-# Create your views here.
+class fstockviewset(viewsets.ModelViewSet):
+    queryset = fstock.objects.all().order_by('Mfd_id')
+    serializer_class = fstockSerializer
