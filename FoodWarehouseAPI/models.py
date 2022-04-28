f5dae7e881b6c9ebd7f739f693fdbb3a52d9c544
JC <ezjcd101@gmail.com>
2022-04-28 18:19:48 +0800

    mudel

--- FoodWarehouseAPI/models.py
+++ FoodWarehouseAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class fstock(models.Model):
+    Mfd_id = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    UnitPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    UnitsInStock = models.PositiveSmallIntegerField()
 
-# Create your models here.
+    def __str__(self):
+        return self.Name
